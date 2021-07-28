0b3d4f28216a3569e301641d68f74c1670515ad4
Nicolas <nicolasrovai@gmail.com>
2021-07-28 10:48:47 -0300

    IBaseRepository created

---
+++ OngProject/OngProject/Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OngProject.Core.Models;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface IBaseRepository<T> where T : EntityBase
+    {
+        bool SaveChanges();
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Create(int id);
+        void Update(int id);
+        void Delete(int id);
+    }
+}
