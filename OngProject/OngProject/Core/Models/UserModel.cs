ffe482681d68922af098142e9c6ab0a8f8f82a83
chiaralina <chiaralinacasadei@gmail.com>
2021-07-28 15:09:02 -0300

    Updated DbContext and UserModel with EntityBase

---
+++ OngProject/OngProject/Core/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Models
+{
+    public class UserModel
+    {
+
+        [MaxLength(255)]
+        [Required]
+        public string firstName { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string lastName { get; set; }
+
+        [MaxLength(320)]
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string password { get; set; }
+
+        [MaxLength(255)]
+        public string photo { get; set; }
+        
+        //[ForeignKey()]
+        public int roleId { get; set; } // Clave foranea hacia ID de Role
+    }
+}
---
+++ OngProject/OngProject/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OngProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserModel>()
+                .HasIndex(u => u.email)
+                .IsUnique();
+        }
+    }
+}
