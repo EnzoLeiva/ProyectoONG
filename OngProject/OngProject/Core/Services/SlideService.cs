152ec95d7890d3e40e5fbb58c311f5602383395d
Agustin Utrera <utrera.agustincba73@gmail..com>
2021-07-28 22:34:58 -0300

    adding  slideService

---
+++ OngProject/OngProject/Core/Services/SlideService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Services
+{
+    public class SlideService
+    {
+
+       public SlideService()
+        {
+
+        }
+
+
+
+    }
+}
