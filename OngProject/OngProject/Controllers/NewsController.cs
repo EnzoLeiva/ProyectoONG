b5a4d0c9d876b8298e8d0faf8f5ef380110a0dfe
Mauro Gutierrez <mauronicolasar@gmail.com>
2021-07-29 22:11:57 -0300

    News Controller and service created

---
+++ OngProject/OngProject/Controllers/NewsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OngProject.Infrastructure.Data;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
