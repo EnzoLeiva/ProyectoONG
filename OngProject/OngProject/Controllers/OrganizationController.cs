b275dfca103332143bbb1cf9a3d50410164e1518
Enzo Leiva <64229954+EnzoLeiva@users.noreply.github.com>
2021-07-29 21:49:24 -0300

    Controller, servicie and repository done

---
+++ OngProject/OngProject/Controllers/OrganizationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OngProject.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrganizationController: ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrganizationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ OngProject/OngProject/Core/Services/OrganizationService.cs
@@ -0,0 +1,16 @@
+﻿using OngProject.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Services
+{
+    public class OrganizationService
+    {
+
+        public OrganizationService()
+        {
+        }
+    }
+}
