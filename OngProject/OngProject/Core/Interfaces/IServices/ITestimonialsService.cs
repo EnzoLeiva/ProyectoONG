e30464115bd8671cd118e1801631b1020cbef5be
Nicolas <nicolasrovai@gmail.com>
2021-07-30 18:21:54 -0300

    TestimonalsController and TestimonialsServices created

---
+++ OngProject/OngProject/Core/Interfaces/IServices/ITestimonialsService.cs
@@ -0,0 +1,12 @@
+﻿using OngProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces.IServices
+{
+    public interface ITestimonialsService
+    {
+    }
+}
