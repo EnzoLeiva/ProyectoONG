93564acd4a64e56608a4d116baad09d0904c4c6d
EricaAcha <erica.acha@gmail.com>
2021-07-30 10:07:52 -0300

    add registration of CategoryService into startUp

--- OngProject/OngProject/Startup.cs
+++ OngProject/OngProject/Startup.cs
@@ -10,6 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OngProject.Core.Interfaces;
+using OngProject.Infrastructure.Repositories;
+using OngProject.Core.Services;
 
 namespace OngProject
 {
@@ -34,6 +37,10 @@ namespace OngProject
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OngProject", Version = "v1" });
             });
+
+           
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            services.AddTransient<ICategoryService, CategoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
