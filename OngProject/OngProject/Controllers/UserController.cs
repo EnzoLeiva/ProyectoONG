3d655ed5da8cb98c4808e258884f48f0cedcb6ee
Stephanie Correa <stephanie.correa24@gmail.com>
2021-08-03 17:02:45 -0300

    jwt configuration added

--- OngProject/OngProject/Controllers/UserController.cs
+++ OngProject/OngProject/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using OngProject.Core.Interfaces.IServices;
 using OngProject.Core.Interfaces.IUnitOfWork;
 using OngProject.Core.Models;
@@ -17,10 +18,11 @@ namespace OngProject.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
-
-        public UserController(IUserService userService)
+        private readonly IConfiguration _configuration;
+        public UserController(IUserService userService, IConfiguration configuration)
         {
             this._userService = userService;
+            this._configuration = configuration;
         }
 
         [HttpDelete("{id}")]
