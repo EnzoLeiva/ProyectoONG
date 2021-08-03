f134faf7d15237b3191db1e86ad982acb7f1132c
Franco <francodejean@gmail.com>
2021-08-03 12:30:09 -0300

    Added CommentService

---
+++ OngProject/OngProject/Core/Services/CommentService.cs
@@ -0,0 +1,31 @@
+﻿using OngProject.Core.DTOs;
+using OngProject.Core.Interfaces.IServices;
+using OngProject.Core.Interfaces.IUnitOfWork;
+using OngProject.Core.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly IUnitOfWork _unitOfWOrk;
+
+        public CommentService(IUnitOfWork unitOfWOrk)
+        {
+            this._unitOfWOrk = unitOfWOrk;
+        }
+        
+        public async Task<IEnumerable<CommentDto>> GetAllComments()
+        {
+            var mapper = new EntityMapper();
+            var commentList = await _unitOfWOrk.CommentRepository.GetAll();
+            var commentDtoList = commentList.Select(c => mapper.FromCommentToCommentDto(c)).ToList();
+
+            return commentDtoList;
+        }
+    }
+}
