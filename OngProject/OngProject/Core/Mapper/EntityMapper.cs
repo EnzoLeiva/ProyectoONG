7effd671ab1e2cd2570d067b3bcd99c64d6fcc42
Franco <francodejean@gmail.com>
2021-08-03 13:12:01 -0300

    Resolve Merge Conflict

--- OngProject/OngProject/Core/Mapper/EntityMapper.cs
+++ OngProject/OngProject/Core/Mapper/EntityMapper.cs
@@ -5,14 +5,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace OngProject.Core.Mapper
+
 {
     public class EntityMapper
     {
+        public SlideDto FromSlideToSlideDto(SlideModel slide)
+        {
+            var slideDto = new SlideDto()
+            {
+                ImageUrl = slide.ImageUrl,
+                Order = slide.Order
+            };
+
+            return slideDto;
+        }
         public CommentDto FromCommentToCommentDto(CommentModel comment)
         {
             var commentDto = new CommentDto() { Body = comment.Body };
+
             return commentDto;
         }
+
     }
+
 }
