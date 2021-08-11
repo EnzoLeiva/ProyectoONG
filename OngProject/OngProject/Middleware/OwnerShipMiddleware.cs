4c3138fb58d5f01107092c0eadf4fd1fb3833798
Stephanie Correa <stephanie.correa24@gmail.com>
2021-08-11 10:08:00 -0300

    added OwnerShip middleware

---
+++ OngProject/OngProject/Middleware/OwnerShipMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OngProject.Middleware
+{
+    public class OwnerShipMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public OwnerShipMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            string path = context.Request.Path;
+
+            if (path.Contains("users"))
+            {
+                string[] splittedPath = path.Split("/");
+
+                var id = context.User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).FirstOrDefault()?.Value;
+
+                var rol = context.User.IsInRole("Admin");
+
+                if (splittedPath[2] == id || rol)
+                {
+                    await _next.Invoke(context);
+                }
+                else
+                {
+                    context.Response.StatusCode = 403;
+                }
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+            
+        }
+    }
+}
