f6c629fe775012e10ade64eefb971c4aeb1b01c1
EricaAcha <erica.acha@gmail.com>
2021-08-11 22:17:40 -0300

    change type of field Image

--- OngProject/OngProject/Core/DTOs/MemberCreateDto.cs
+++ OngProject/OngProject/Core/DTOs/MemberCreateDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -23,7 +24,7 @@ namespace OngProject.Core.DTOs
 
         [Required]
         [MaxLength(255)]
-        public string Image { get; set; }
+        public IFormFile Image { get; set; }
 
         [MaxLength(255)]
         public string Description { get; set; }
