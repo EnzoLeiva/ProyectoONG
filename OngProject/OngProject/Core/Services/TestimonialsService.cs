66b45392f1a8c3641798ec7abe7485db45b806e5
EricaAcha <erica.acha@gmail.com>
2021-08-11 22:57:18 -0300

    add getById method and imagenService to Delete

--- OngProject/OngProject/Core/Services/TestimonialsService.cs
+++ OngProject/OngProject/Core/Services/TestimonialsService.cs
@@ -1,4 +1,5 @@
 ﻿using OngProject.Core.Interfaces.IServices;
+using OngProject.Core.Interfaces.IServices.AWS;
 using OngProject.Core.Interfaces.IUnitOfWork;
 using OngProject.Core.Models;
 using System;
@@ -11,16 +12,21 @@ namespace OngProject.Core.Services
     public class TestimonialsService : ITestimonialsService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IImagenService _imagenService;
 
-        public TestimonialsService(IUnitOfWork unitOfWork)
+        public TestimonialsService(IUnitOfWork unitOfWork, IImagenService imagenService)
         {
             _unitOfWork = unitOfWork;
+            _imagenService = imagenService;
         }
 
         public async Task<bool> Delete(int id)
         {
             try
             {
+                TestimonialsModel testimonials = await GetById(id);
+                await _imagenService.Delete(testimonials.Image);
+
                 await _unitOfWork.TestimonialsRepository.Delete(id);
                 await _unitOfWork.SaveChangesAsync();
             }
@@ -29,6 +35,11 @@ namespace OngProject.Core.Services
                 return false;
             }
             return true;
+        }
+
+        public async Task<TestimonialsModel> GetById(int id)
+        {
+            return await _unitOfWork.TestimonialsRepository.GetById(id);
         }
 
         public bool EntityExist(int id)
