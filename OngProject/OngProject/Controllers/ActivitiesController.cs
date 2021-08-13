780eb4120637cd521d6bc9fe47197ef12d9feb42
Franco <francodejean@gmail.com>
2021-08-13 12:22:16 -0300

    Resolve some Problems in User Authorize

--- OngProject/OngProject/Controllers/ActivitiesController.cs
+++ OngProject/OngProject/Controllers/ActivitiesController.cs
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-=======
-﻿using Microsoft.AspNetCore.Mvc;
 using OngProject.Core.DTOs;
->>>>>>> 14e0f52c6ea86348ed7078ad28918db708c43f4f
 using OngProject.Core.Interfaces.IServices;
 using OngProject.Core.Services;
 using System;
