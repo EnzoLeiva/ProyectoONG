ba0af457a9fa4dc5794f6e346927576806555877
EricaAcha <erica.acha@gmail.com>
2021-08-17 16:32:27 -0300

    add pagination helper

---
+++ OngProject/OngProject/Core/Helper/Pagination/ResponsePagination.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Helper.Pagination
+{
+    public class ResponsePagination<T>
+    {
+        public ResponsePagination(T data)
+        {
+            Data = data;
+        }
+
+    
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+        public bool HasNextPage { get; set; }
+        public bool HasPreviousPage { get; set; }
+        public string NextPageUrl { get; set; }
+        public string PreviousPageUrl { get; set; }
+        public T Data { get; set; }
+    }
+}
