026615789df9dca46a1fe7bd41399c60ffa9ed5a
Enzo Leiva <64229954+EnzoLeiva@users.noreply.github.com>
2021-08-19 23:01:34 -0300

    Conflict fixed 1.1

--- OngProject/OngProject/Core/Mapper/EntityMapper.cs
+++ OngProject/OngProject/Core/Mapper/EntityMapper.cs
@@ -421,5 +421,28 @@ namespace OngProject.Core.Mapper
 
             return organization;
         }
+
+        public SlideModel FromSlideUpdateDtoToSlide(SlideUpdateDto slideUpdateDto, SlideModel slide)
+        {
+
+            if (slideUpdateDto.Image != null)
+
+                slide.ImageUrl = GetNameImage("slide");
+
+            if (slideUpdateDto.Order != null)
+
+                slide.Order = (int)slideUpdateDto.Order;
+
+            if (!string.IsNullOrEmpty(slideUpdateDto.Text))
+
+                slide.Text = slideUpdateDto.Text;
+
+            if (!string.IsNullOrEmpty(slideUpdateDto.OrganizationId))
+
+                slide.OrganizationId = slideUpdateDto.OrganizationId;
+
+            return slide;
+
+        }
     }
 }
