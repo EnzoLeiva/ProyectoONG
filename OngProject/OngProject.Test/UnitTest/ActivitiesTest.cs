3d2729069cd00f80e63d848fa75f67a736e407f6
Emmanuel Ranone <emmanuelranone@hotmail.com>
2021-08-23 11:30:47 -0300

    add unit test project add helper and unit test folder add inMemmory Nuget

---
+++ OngProject/OngProject.Test/UnitTest/ActivitiesTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngProject.Test.UnitTest
+{
+    [TestClass]
+    public class ActivitiesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arranger
+            
+
+            // Act
+            
+
+            // Assert
+
+        }
+    }
+}
